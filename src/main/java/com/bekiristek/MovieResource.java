ae528a102935dd08b9e437c7b6d56cb0b2ce62e9
Bekir İstek <bekir.istek@gmail.com>
2022-01-08 19:50:48 +0300

    init

---
+++ src/main/java/com/bekiristek/MovieResource.java
@@ -0,0 +1,34 @@
+package com.bekiristek;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/movies")
+public class MovieResource {
+    public static List<String> movies = new ArrayList<>();
+
+    @GET()
+    @Produces (MediaType.TEXT_PLAIN)
+    public Response getMovies(){
+        return Response.ok(movies).build();
+    }
+
+    @GET
+    @Produces (MediaType.TEXT_PLAIN)
+    @Path("/size")
+    public Integer countMovies(){
+        return movies.size();
+    }
+
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response addMovie(String newMovie){
+        movies.add(newMovie);
+        return Response.ok(movies).build();
+
+    }
+}
